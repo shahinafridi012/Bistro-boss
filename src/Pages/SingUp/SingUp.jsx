8422fbbb712624fbfabc8c89189845434c21cece
unknown <shahinafridi012@gmail.com>
2024-09-06 21:40:19 +0530

    i am back after 1 week

--- src/Pages/SingUp/SingUp.jsx
+++ src/Pages/SingUp/SingUp.jsx
@@ -1,9 +1,53 @@
 
-
+import img from '../../assets/others/authentication2.png'
 const SingUp = () => {
     return (
-        <div>
-            
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left lg:w-[50%] w-full">
+                   <img src={img} alt="" />
+                </div>
+                <div className="card bg-base-100 rounded-xl w-full lg:w-[50%] shrink-0 shadow-2xl ">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="name" name='name' className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="Email" name='email' className="input input-bordered" required />
+                            <label className="label">
+                             
+                            </label>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo</span>
+                            </label>
+                            <input type="text" placeholder="Your Photo" name='photo' className="input input-bordered" required />
+                            <label className="label">
+                            
+                            </label>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" name='password' className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
